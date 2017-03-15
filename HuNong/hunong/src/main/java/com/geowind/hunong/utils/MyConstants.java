1af98b0cd77a5364ce06d456bb26c2b231d700ed
logaxy <184369099@qq.com>
2017-03-15 17:13:08 +0800

    修改病虫反馈URL

--- HuNong/hunong/src/main/java/com/geowind/hunong/utils/MyConstants.java
+++ HuNong/hunong/src/main/java/com/geowind/hunong/utils/MyConstants.java
@@ -24,7 +24,7 @@ public interface MyConstants {
     String HOMEURL="";//主页数据的URL
     String CACHED_APP_KEY="cachedAppkey";//缓存的appkey
     //病虫害反馈及专家咨询处URL，服务器地址
-    String PEST_OR_CONSULT_UPLOAD_URL="http://192.168.1.113:8080/MutualAgriculture/pestOrConsultInfoUploadServlet";
+    String PEST_OR_CONSULT_UPLOAD_URL="http://115.159.125.122:8080/MutualAgriculture/pestOrConsultInfoUploadServlet";
     String REGISTER="http://115.159.125.122:8080/MutualAgriculture/UserServlet";//注册的
     String GETCENTER="http://115.159.125.122:8080/MutualAgriculture/UserServlet";//获取服务中心
     String LIBRARYSEARCH="http://115.159.125.122:8080/MutualAgriculture/LibraryServlet";//文库搜索
@@ -33,12 +33,12 @@ public interface MyConstants {
     String TABLE_TASK="task";//任务表名
     String TABLE_SYSTEM_MSG="system_msg";//系统消息表
     String TABLE_EXPERT_REPLY="expert_reply";//专家回复
-<<<<<<< Updated upstream
+
     String IS_LIGHT="isLight";//是否闪烁呼吸灯
     String IS_VIBRATE="isVibrate";//是否震动
     String USER_TYPE="userType";//用户类型
-=======
+
 
     String VERSION="V1.0.0";
->>>>>>> Stashed changes
+
 }
